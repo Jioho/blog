81bcd82d842b7e7a4ee22634a266df14dae5dc8a
Jioho <jioho_chen@163.com>
2020-06-28 13:33:41 +0800

    添加插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,9 +10,14 @@ const plugins = [
   'rpurl',
   '@vuepress/back-to-top',
   'vuepress-plugin-zooming',
+  'vuepress-plugin-baidu-autopush',
   ['autobar', autobarConfig],
+  ['vuepress-plugin-code-copy', true],
   ['vuepress-plugin-helper-live2d', live2dConfig]
 ]
+
+//  'vuepress-plugin-baidu-autopush'
+// ['vuepress-plugin-code-copy', true]
 if (process.env.NODE_ENV !== 'development') {
   plugins.push(['vuepress-plugin-mygitalk', myTalkConfig])
 }
@@ -26,7 +31,22 @@ module.exports = {
   },
   title: 'knowledge',
   description: "Let's learn together",
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  head: [
+    [
+      'script',
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?21b6775b18360c4e8a93122bc08e4af4";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ],
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   themeConfig: {
     sidebar: 'auto', // 自动生成侧边栏
     displayAllHeaders: false, // 显示所有页面的标题链接
